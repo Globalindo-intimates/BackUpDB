b7755e50998b06f3054c981b43ee0b568c097abc
heriipurnama <herii.sinatra@gmail.com>
2020-08-11 11:24:31 +0700

    modify

--- Script/BackUpDB.sh
+++ Script/BackUpDB.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-mysqldump -u manager  -p@Gl0b4l1nd0 dbMaster | gzip > /home/manager/BackUpDB/BackUp_file/database_Report-GI_`date '+%d-%m-%Y'`.sql.gz
+mysqldump -u Root  -pbash dbMaster | gzip > /home/manager/BackUpDB/BackUp_file/database_Report-GI_`date '+%d-%m-%Y'`.sql.gz
